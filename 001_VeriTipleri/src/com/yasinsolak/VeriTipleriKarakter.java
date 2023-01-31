85d423f6671921097f0c69f7141433cfee5eac13
ysnslk <yasin.solak.26@gmail.com>
2023-01-31 12:30:37 +0300

    VeriTipleriSwitchCase

---
+++ 001_VeriTipleri/src/com/yasinsolak/VeriTipleriKarakter.java
@@ -0,0 +1,35 @@
+package com.yasinsolak;
+
+public class VeriTipleriKarakter {
+
+	public static void main(String[] args) {
+		
+		//char veritipi :
+		//1 Byte
+		char charValue = 'A'; //ASCII tablosundaki karşılığı 65
+		System.out.println(charValue);
+		
+		//Soru 
+		char charValue2 = 65; // Ondalık değerini atsan bile karşılığı yazar
+		System.out.println(charValue2);
+		
+		char charValue3 = 84;
+		System.out.println(charValue3);
+		
+		
+		
+		//String veritipi
+		
+		//Ekrana yazdırmak için syso("Ali Kaya")
+		System.out.println("Ali Veli");
+		
+		String name = "Ali";
+		System.out.println(name);
+		String sname = "Veli";
+		System.out.println("Isim : "+name+" Soyisim : "+sname);
+		
+		
+		
+		
+	}
+}
