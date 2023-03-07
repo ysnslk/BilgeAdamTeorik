8e6e3b15c1b5faac6934d83123ee2a9edde077ac
ysnslk <yasin.solak.26@gmail.com>
2023-03-07 08:13:54 +0300

    update

--- 040_FileInputStream/src/com/yasinsolak/Main.java
+++ 040_FileInputStream/src/com/yasinsolak/Main.java
@@ -3,6 +3,7 @@ package com.yasinsolak;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class Main {
 
@@ -108,24 +109,55 @@ public class Main {
 			}
 		}
 	}
-	
-	public static void dosyaninBelirtilenKisminiOku(int start , int howMuch) {
-		
+
+	public static void dosyaninBelirtilenKisminiOku() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Kaçıncı karakterden başlasın");
+		int start = sc.nextInt();
+		System.out.println("Kaç karakter yazsın");
+		int howMuch = sc.nextInt();
+		FileInputStream fileInputStream = null;
+		try {
+			fileInputStream = new FileInputStream("dosya.txt");
+
+			fileInputStream.skip(start-1);
+			for (int i = 0; i < howMuch; i++) {
+				System.out.print((char) fileInputStream.read());
+			}
+			System.out.println();
+
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+			System.out.println("Dosya bulunamadı...");
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.out.println("Dosya okuma sırasında hata oluştu...");
+		} catch (Exception e) {
+			System.out.println("Bilinmeyen bir hata oluştu");
+		} finally {
+			try {
+				if (fileInputStream != null) {
+					fileInputStream.close();
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+				System.out.println("Dosya kapatılırken hata oldu");
+			}
+		}
 	}
 
 	public static void main(String[] args) {
 
 		// dosyadanTekbirByteOkuma();
-		//dosyaninOrtasindanByteOkuma();
-		dosyaninTumunuOkuma();
-		
+		// dosyaninOrtasindanByteOkuma();
+		//dosyaninTumunuOkuma();
+		dosyaninBelirtilenKisminiOku();
+
 		/*
-		 * Ödev :
-		 * Bir metod yazınız
-		 * Bir dosyanın n. karakterinden başlayıp m tane karakter okusun ve ekrana yazsın
-		 * dosyaninBelirtilenKisminiOku
+		 * Ödev : Bir metod yazınız Bir dosyanın n. karakterinden başlayıp m tane
+		 * karakter okusun ve ekrana yazsın dosyaninBelirtilenKisminiOku
 		 */
-		
+
 		System.out.println("Hoşçakalın");
 
 	}
