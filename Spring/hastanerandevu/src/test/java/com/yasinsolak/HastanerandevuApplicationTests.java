7113b50548ce9d62002f5f5be42bb02b998dd03e
ysnslk <yasin.solak.26@gmail.com>
2023-06-09 17:00:38 +0300

    9-06-23 Gün Sonu

---
+++ Spring/hastanerandevu/src/test/java/com/yasinsolak/HastanerandevuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yasinsolak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HastanerandevuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
